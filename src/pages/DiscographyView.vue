35df9a9c302725889e2f5b6101d4874d086f1182
B4Lee <iqbalanggoro48@gmail.com>
2023-01-19 22:51:32 +0700

    Update Discography

--- src/pages/DiscographyView.vue
+++ src/pages/DiscographyView.vue
@@ -4,14 +4,16 @@ import Navbar from '../components/Navbar.vue'
 
 <template>
     <Navbar />
-
     <div>
-        <h1 class="text-center">Moccatune Works</h1>
+    <h1 class="text-center">Moccatune Works</h1>
     <p class="text-center"><strong>Discography</strong></p>
     <div class="row">
-      <div class="col-lg-4"><img src="../assets/images/moccatune1.jpg" class="img-thumbnail img-fluid" alt="Placeholder image"></div>
-      <div class="col-lg-4"><img src="../assets/images/moccatune3.jpg" class="img-thumbnail img-fluid" alt="Placeholder image"></div>
-      <div class="col-lg-4"><img src="../assets/images/moccatune-lelah.jpg" class="img-thumbnail img-fluid" alt="Placeholder image"></div>
+      <div class="col-lg-4"><img src="../assets/images/discography/moccatune-cuma-rindu-single.jpg" class="img-thumbnail img-fluid" alt="Placeholder image"></div>
+      <div class="col-lg-4"><img src="../assets/images/discography/moccatune-disampingmu.png" class="img-thumbnail img-fluid" alt="Placeholder image"></div>
+      <div class="col-lg-4"><img src="../assets/images/discography/moccatune-sore-ini-untuk-selamanya.jpg" class="img-thumbnail img-fluid" alt="Placeholder image"></div>
+      <div class="col-lg-4"><img src="../assets/images/discography/moccatune-diam-diam-aku-suka.jpg" class="img-thumbnail img-fluid" alt="Placeholder image"></div>
+      <div class="col-lg-4"><img src="../assets/images/discography/moccatune-lelah.jpg" class="img-thumbnail img-fluid" alt="Placeholder image"></div>
+      <div class="col-lg-4"><img src="../assets/images/discography/moccatune-tentangmu.jpg" class="img-thumbnail img-fluid" alt="Placeholder image"></div>
     </div>
     </div>
 </template>
