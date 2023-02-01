13bf66360cd330ea138872fd539ecd655b4efce0
B4Lee <iqbalanggoro48@gmail.com>
2023-02-01 20:30:35 +0700

    change background
    fix background responsiveness

--- src/pages/AboutView.vue
+++ src/pages/AboutView.vue
@@ -10,7 +10,7 @@ import Navbar from '../components/Navbar.vue'
     <h1 class="text-center"><em>Tentang Kami</em></h1>
     <p class="text-center">Moccatune adalah Content Creator Group Pertama di Indonesia yang mengusung konten berwujud Storytelling Music Video dengan Ilustrasi bergaya Manga atau Komik! Berusaha konsisten dalam berkarya, moccatune menciptakan sebuah Konsep dimana musik dan cerita menjadi satu dalam sebuah dunia yang bernama Moccatune Universe, dimana kamu dapat mengikuti kisah Baim, Anin dan kawan-kawan dalam menjalani kehidupan di masa remaja! ❤</p>
     <br>
-    <div class="jumbotron"><img src="../assets/images/Untitled.png" class="img-fluid" alt="Placeholder image"></div>
+    <div class="about-bg"><img src="../assets/images/Untitled.png" class="img-fluid" alt="Placeholder image"></div>
     <br>
   </div>
 </template>
--- src/pages/CharacterView.vue
+++ src/pages/CharacterView.vue
@@ -6,7 +6,7 @@ import Navbar from '../components/Navbar.vue'
 <template>
     <Navbar />
     <div class="jumbotron">
-      <div id="carouselExampleIndicators1" class="carousel slide" data-ride="carousel" style="background-color: grey"> 
+      <div id="carouselExampleIndicators1" class="carousel slide" data-ride="carousel"> 
         <!--
         <ol class="carousel-indicators">
           <li data-target="#carouselExampleIndicators1" data-slide-to="0" class="active"></li>
@@ -16,12 +16,12 @@ import Navbar from '../components/Navbar.vue'
 -->
         <div class="carousel-inner" role="listbox">
           <div class="carousel-item active"> <img class="d-block mx-auto img-fluid" src="../assets/images/kemal-satrianda.webp" alt="First slide"> </div>
-          <div class="carousel-item"> <img class="d-block mx-auto" src="../assets/images/adam-raditya.webp" alt="Second slide"> </div>
-          <div class="carousel-item"> <img class="d-block mx-auto" src="../assets/images/nadia-calista.webp" alt="Third slide"> </div>
-          <div class="carousel-item"> <img class="d-block mx-auto" src="../assets/images/anindita-putri.webp" alt="Third slide"> </div>
-          <div class="carousel-item"> <img class="d-block mx-auto" src="../assets/images/ibrahim-oktavian.webp" alt="Third slide"> </div>
-          <div class="carousel-item"> <img class="d-block mx-auto" src="../assets/images/wahyu-dirgantara.webp" alt="Third slide"> </div>
-          <div class="carousel-item"> <img class="d-block mx-auto" src="../assets/images/diana-puspitasari.webp" alt="Third slide"> </div>
+          <div class="carousel-item"> <img class="d-block mx-auto img-fluid" src="../assets/images/adam-raditya.webp" alt="Second slide"> </div>
+          <div class="carousel-item"> <img class="d-block mx-auto img-fluid" src="../assets/images/nadia-calista.webp" alt="Third slide"> </div>
+          <div class="carousel-item"> <img class="d-block mx-auto img-fluid" src="../assets/images/anindita-putri.webp" alt="Third slide"> </div>
+          <div class="carousel-item"> <img class="d-block mx-auto img-fluid" src="../assets/images/ibrahim-oktavian.webp" alt="Third slide"> </div>
+          <div class="carousel-item"> <img class="d-block mx-auto img-fluid" src="../assets/images/wahyu-dirgantara.webp" alt="Third slide"> </div>
+          <div class="carousel-item"> <img class="d-block mx-auto img-fluid" src="../assets/images/diana-puspitasari.webp" alt="Third slide"> </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleIndicators1" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only">Previous</span> </a> <a class="carousel-control-next" href="#carouselExampleIndicators1" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only">Next</span> </a> </div>
     </div>
